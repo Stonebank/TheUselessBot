4b8fef44ae02f1e5944f9e9628402201c3f6a6f7
Hassan <hassan_99@live.dk>
2020-06-28 14:39:18 +0200

    OSRS highscore command, fetches data from OSRS highscores, commands list generates via Hastebin.kt

--- src/main/java/discord/configuration/DiscordConfig.java
+++ src/main/java/discord/configuration/DiscordConfig.java
@@ -11,8 +11,6 @@ public class DiscordConfig {
 
     public static final String TOKEN = Config.getConfig().getProperty("token");
 
-    public static final String PASTEBIN_TOKEN = Config.getConfig().getProperty("pastebin_token");
-
     public static final GatewayIntent[] intent = new GatewayIntent[] { GatewayIntent.GUILD_EMOJIS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.DIRECT_MESSAGE_TYPING, GatewayIntent.DIRECT_MESSAGE_REACTIONS };
 
     public static final Path OSRS_ITEM_DB = Paths.get(Config.getConfig().getProperty("osrs_db_path"));
