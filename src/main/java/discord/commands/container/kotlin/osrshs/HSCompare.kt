550fad70be24b472a69a9bffdc18c6c7d3f141bc
Hassan <hassan_99@live.dk>
2020-06-28 23:32:53 +0200

    OSRS highscore command, OSRS compare command (Temporarily), fetches data from OSRS highscores, commands list generates via Hastebin.kt

--- src/main/java/discord/commands/container/kotlin/osrshs/HSCompare.kt
+++ src/main/java/discord/commands/container/kotlin/osrshs/HSCompare.kt
@@ -4,8 +4,10 @@ import discord.commands.DiscordCommand
 import discord.entity.DiscordUser
 import discord.entity.highscore.Highscore
 import discord.entity.highscore.Skills
+import discord.utils.Utils
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.awt.Color
 
 class HSCompare : DiscordCommand() {
 
@@ -32,7 +34,7 @@ class HSCompare : DiscordCommand() {
         val p1XP = LongArray(Skills.values().size)
         val p2XP = LongArray(Skills.values().size)
 
-        val embed = EmbedBuilder()
+        val embed = EmbedBuilder().setTitle("${p1?.name} (rank: ${Utils.formatNumber(p1?.getRank())}) vs ${p2?.name} (rank: ${Utils.formatNumber(p2?.getRank())})").setColor(Color((0..255).random(), (0..255).random(), (0..255).random())).setThumbnail("http://www.theoslounge.com/wp-content/uploads/2020/02/skillicon.png")
 
         for (skill in Skills.values()) {
 
@@ -42,17 +44,17 @@ class HSCompare : DiscordCommand() {
             p1XP[skill.ordinal] = p1.getSkillExperience(skill)
             p2XP[skill.ordinal] = p2.getSkillExperience(skill)
 
-            if (p1Stats[skill.ordinal] > p2Stats[skill.ordinal])
-                embed.appendDescription("${p1.name} has better stats in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
-            else if (p2Stats[skill.ordinal] > p1Stats[skill.ordinal])
-                embed.appendDescription("${p2.name} has better stats in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
-            else if (p1Stats[skill.ordinal] == p2Stats[skill.ordinal]) {
-                if (p1XP[skill.ordinal] > p2XP[skill.ordinal])
-                    embed.appendDescription("${p1.name} has better xp in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
-                else if (p2XP[skill.ordinal] > p1XP[skill.ordinal])
-                    embed.appendDescription("${p2.name} has better xp in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
-                else
-                    embed.appendDescription("Both are equal in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}")
+            when {
+                p1Stats[skill.ordinal] > p2Stats[skill.ordinal] -> embed.appendDescription("${p1.name} has better stats in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
+                p2Stats[skill.ordinal] > p1Stats[skill.ordinal] -> embed.appendDescription("${p2.name} has better stats in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
+            }
+
+            if (p1Stats[skill.ordinal] == p2Stats[skill.ordinal]) {
+                when {
+                    p1XP[skill.ordinal] > p2XP[skill.ordinal] -> embed.appendDescription("${p1.name} has better xp in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
+                    p2XP[skill.ordinal] > p1XP[skill.ordinal] -> embed.appendDescription("${p2.name} has better xp in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}\n")
+                    else -> embed.appendDescription("Both are equal in ${skill.emoji} ${skill.name.toLowerCase().capitalize()}")
+                }
             }
 
         }
