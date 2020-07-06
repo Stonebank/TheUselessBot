53c2d7414d6db92d77b40c2bfa100fd7e685474e
Hassan <hassan_99@live.dk>
2020-07-06 11:50:03 +0200

    Forbes and Jeff Bezos commands

--- src/main/java/discord/commands/container/kotlin/fun/JeffBezos.kt
+++ src/main/java/discord/commands/container/kotlin/fun/JeffBezos.kt
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import org.jsoup.Jsoup
 import java.lang.management.ManagementFactory
+import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit
 
 class JeffBezos : DiscordCommand() {
@@ -31,8 +32,12 @@ class JeffBezos : DiscordCommand() {
         println(TimeUnit.MILLISECONDS.toSeconds(uptime))
 
         val connection = Jsoup.connect("https://www.forbes.com/profile/jeff-bezos/").userAgent("Mozilla/5.0").get().select("div.profile-info__item-value-container")
+        val date = SimpleDateFormat("dd-M-yyyy hh:mm:ss").parse("26-05-2020 9:41:00")
 
-        bot?.channel?.sendMessage("The bot has been online for ${Utils.formatTime(TimeUnit.MILLISECONDS.toSeconds(uptime))}and Jeff Bezos has earned $${Utils.formatNumber(TimeUnit.MILLISECONDS.toSeconds(uptime) * 2489)} USD, his total networth is ${connection.text()}")?.queue()
+        bot?.channel?.sendMessage("Since the creation of the useless bot, Jeff Bezos has earned $${Utils.formatNumber(TimeUnit.MILLISECONDS.toSeconds(date.time) * 2489.toLong())} USD\nThe bot has been online for ${Utils.formatTime(TimeUnit.MILLISECONDS.toSeconds(uptime))}and Jeff Bezos has earned $${Utils.formatNumber(TimeUnit.MILLISECONDS.toSeconds(uptime) * 2489.toLong())} USD\nTotal networth is ${connection.text()}")?.queue()
+
+        println(date.time)
+
     }
 
 }
